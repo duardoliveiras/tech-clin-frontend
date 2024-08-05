691bd38e7a0f401a15749bbcf1dda36a5990ec2e
duardoliveiras <duardooliveiras@gmail.com>
2024-08-05 12:31:03 -0400

    feat: implementando slider de imagens para clinicas e organizando estilos do homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
-import { LogIn } from "lucide-react";
+import {
+  AlarmClock,
+  Check,
+  ChevronLeft,
+  ChevronRight,
+  LayoutGrid,
+  LogIn,
+  Stethoscope,
+} from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -10,36 +18,64 @@ export default function Home() {
             <Image src={"/techclin.svg"} width={60} height={60} alt={"Logo"} />
           </div>
 
-          <div className="w-full justify-center gap-x-3 items-center flex flex-row">
-            <div className="w-auto h-auto text-xs bg-secondary rounded-lg p-3 flex items-center justify-center">
-              Início
-            </div>
-            <div className="w-auto h-auto text-xs bg-secondary rounded-lg p-3 flex items-center justify-center">
-              Parceiros
-            </div>
-            <div className="w-auto  h-auto text-xs bg-secondary rounded-lg p-3 flex items-center justify-center">
-              Contato
-            </div>
-            <div className="w-auto  h-auto text-xs bg-secondary rounded-lg p-3 flex items-center justify-center">
-              Agendar
-            </div>
+          <div className="w-full justify-center gap-x-5 text-white items-center flex flex-row">
+            <p className="cursor-pointer hover:text-green-600">inicio</p>
+            <p className="cursor-pointer hover:text-green-600">parceiros</p>
+            <p className="cursor-pointer hover:text-green-600">contato</p>
+            <p className="cursor-pointer hover:text-green-600">agendar</p>
           </div>
           <div className="w-auto">
-            <div className="w-auto  h-auto text-xs bg-third gap-x-2 rounded-lg p-2 flex items-center justify-center">
+            <div className="w-auto  h-auto text-xs text-white hover:text-green-600 cursor-pointer gap-x-2 rounded-lg p-2 flex items-center justify-center">
               <LogIn />
               <p>Entrar</p>
             </div>
           </div>
         </div>
       </div>
-      <div className="w-full h-full bg-white flex flex-col p-20">
-        <div className="w-full h-auto border-2">
-          <Image
-            src={"/assets/doctors.svg"}
-            width={500}
-            height={500}
-            alt="Médicos"
-          />
+      <div className="w-full h-full bg-white flex flex-col p-16">
+        <div className="w-full h-auto flex flex-row">
+          <div className="w-1/2 flex overflow-hidden">
+            <Image
+              src={"/assets/doctors.svg"}
+              width={500}
+              height={500}
+              alt="Médicos"
+            />
+          </div>
+          <div className="w-full flex flex-col ">
+            <div className="w-full flex items-center pl-5 gap-x-2 text-3xl text-green-700 uppercase">
+              <Stethoscope size={40} />
+              <p>sua saúde em primeiro lugar</p>
+            </div>
+            <div className="mt-5 w-full pl-10 gap-y-10 text-2xl flex flex-col">
+              <div className="w-full items-center gap-x-2  flex">
+                <AlarmClock className="text-green-700" />
+                <p>Facilidade em marcar suas consultas e exames.</p>
+              </div>
+              <div className="w-full items-center gap-x-2  flex">
+                <Check className="text-green-700" />
+                <p>Marque quando quiser e de onde estiver.</p>
+              </div>
+              <div className="w-full items-center gap-x-2  flex">
+                <LayoutGrid className="text-green-700" />
+                <p>Tudo em um só lugar.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-64 flex flex-row bg-green-700 mt-20 rounded-lg overflow-hidden">
+          <div className="w-1/12 h-full flex bg-third justify-center items-center text-white">
+            <ChevronLeft />
+          </div>
+          <div className="w-full h-full bg-secondary flex flex-col items-center p-3">
+            <div className="font-semibold text-white">Clínicas</div>
+            <div className="w-1/2 h-full bg-gray-500 flex justify-center items-center">
+              imagem clínica
+            </div>
+          </div>
+          <div className="w-1/12 h-full flex bg-third justify-center items-center text-white">
+            <ChevronRight />
+          </div>
         </div>
       </div>
     </main>
