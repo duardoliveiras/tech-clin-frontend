356b4d788f1a7c3be92b2fb81066a0a0e73c16f8
duardoliveiras <duardooliveiras@gmail.com>
2024-09-03 12:41:50 -0400

    feat: implementando post de novo usuario

--- src/app/login/Register.tsx
+++ src/app/login/Register.tsx
@@ -1,13 +1,56 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+interface iRegister {
+  name: string;
+  email: string;
+  password: string;
+}
 
 export const Register = () => {
+  const [register, setRegister] = useState<iRegister>({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState<string>();
+
+  const fetchRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      console.log(register);
+      const response = await fetch("http://localhost:4000/criar-usuario", {
+        method: "POST",
+        headers: {
+          Accept: "*/*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(register),
+      });
+      console.log(response);
+      const data = await response.json();
+
+      if (!response.ok) {
+        setError("Erro ao realizar a requisição");
+        return;
+      }
+      window.alert("Usuário criado com sucesso!");
+    } catch (err: any) {
+      //setError(err.message);
+      console.error(err);
+    }
+  };
+
   return (
     <div className="flex w-1/2 h-full justify-center items-center">
-      <form className="flex flex-col bg-white border text-black border-gray-300 w-[80%]  shadow-md rounded-lg py-2">
+      <form
+        className="flex flex-col bg-white border text-black border-gray-300 w-[80%]  shadow-md rounded-lg py-2"
+        onSubmit={fetchRegister}
+      >
         <div className="w-full justify-center flex">
           <h1 className="text-2xl font-bold text-black">Cadastre-se Já</h1>
         </div>
+        <p className="text-red-500">{error}</p>
         <div className="flex flex-col w-full gap-y-8 p-10">
           <div className="flex flex-col w-full px-2">
             <p className="font-bold text-black">Nome:</p>
@@ -15,6 +58,12 @@ export const Register = () => {
               className="w-full h-10 rounded-lg border-blue-800 border px-2"
               type="text"
               placeholder="John Smith"
+              onChange={(e) =>
+                setRegister((prev) => ({
+                  ...prev,
+                  name: e.target.value,
+                }))
+              }
             />
           </div>
           <div className="flex flex-col w-full px-2">
@@ -23,6 +72,12 @@ export const Register = () => {
               className="w-full h-10 rounded-lg border-blue-800 border px-2"
               type="email"
               placeholder="seunome@email.com"
+              onChange={(e) =>
+                setRegister((prev) => ({
+                  ...prev,
+                  email: e.target.value,
+                }))
+              }
             />
           </div>
           <div className="flex flex-col w-full px-2">
@@ -30,6 +85,12 @@ export const Register = () => {
             <input
               className="w-full h-10 rounded-lg border-blue-800 border px-2"
               type="password"
+              onChange={(e) =>
+                setRegister((prev) => ({
+                  ...prev,
+                  password: e.target.value,
+                }))
+              }
             />
           </div>
         </div>
